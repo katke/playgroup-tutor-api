54681188c5125e945f6e3887c779138faf46f3cd
dave-a-ryan <daveryan145@gmail.com>
2021-11-01 10:52:03 -0500

    locations will find by zip, to make creating/editing your account simpler

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,8 +3,13 @@ class LocationsController < ApplicationController
     locations = Location.all
     render json: locations
   end
+
   def show
-    locations = Location.find_by(id: params[:id])
-    render json: locations
+    locations = Location.find_by(zipcode: params[:id])
+    if locations != nil
+      render json: locations
+    else
+      render json: { errors: "Please enter a valid zip code"}, status: :unprocessable_entity
+    end
   end
 end
